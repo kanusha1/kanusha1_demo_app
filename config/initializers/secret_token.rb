bd878a55c4a314188d9c3d645f10fdd476c71ba8
kanusha1 <kanusha1.email@gmail.com>
2014-09-07 18:49:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kanusha1DemoApp::Application.config.secret_key_base = '6a3b88df2bb549acb0fea5168510ed8e3523fbbc9953502f7ccc5b693ada8119dcbfa8df8e447e3932b0a7d1c6e253af2b6714275855469a87e8bbee4c4b7cf6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kanusha1DemoApp::Application.config.session_store :cookie_store, key: '_kanusha1_demo_app_session'
